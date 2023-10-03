83501be2f4c921b1b341b17343e37c4cff303e6c
texotek <fdahmen@proton.me>
2023-10-03 21:00:58 +0200

    finished level1

--- Bakery/src/felix.cpp
+++ Bakery/src/felix.cpp
@@ -9,10 +9,25 @@ int main (int argc, char *argv[]) {
     vector<Day> earnings{};
     vector<Day> payments{};
 
-    string input{};
-    getline(cin, input);
-    cout << input;
-
-    
-    return 0;
+    string text{};
+        do {
+           cin >> text; 
+            
+           if(text == "F") {
+               cin >> text;
+               cin >> text;
+               Day d;
+               d.money = stoi(text);
+               earnings.push_back(d);
+           }
+           if(text == "B") {
+               cin >> text;
+               cin >> text;
+               Day d;
+               d.money = stoi(text);
+               payments.push_back(d);
+           }
+    } while(text != "exit");
+    cout << level1(earnings, payments);
+   return 0;
 }
