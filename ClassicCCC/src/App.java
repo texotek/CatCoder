981f3b0820e342580bbf143f17a12a93794ae104
texotek <fdahmen@proton.me>
2023-10-20 16:16:54 +0200

    level2 ongoing

--- ClassicCCC/src/App.java
+++ ClassicCCC/src/App.java
@@ -3,20 +3,51 @@ import java.io.*;
 public class App {
 
     public static boolean example = false;
-
+    public static int level = 2;
     public static void main(String[]args) throws Exception {
-        File level = new File("level1");
+        File level = new File("level2");
         File[] testfiles = level.listFiles();
 
         for (File f : testfiles){
             if(f.getAbsolutePath().contains(".out")) continue;
             if(example && !f.getAbsolutePath().contains("example")) continue;
+            if(!f.getAbsolutePath().contains("2_1")) continue;
 
             Scanner s = new Scanner(f);
+            List<List<Character>> field = new ArrayList<>();
 
+            int length = s.nextInt();
+
+            s.nextLine();
+            for(int i = 0; i < length; i++) {
+                String line = s.nextLine();
+                List<Character> row = new ArrayList<>();
+                for(int j = 0; j < line.length(); j++) {
+                    row.add(line.charAt(j));
+                }
+                field.add(row);
+            }
+
+
+            Field fieldO = new Field(field);
+            int numOfCoordiantes = s.nextInt();
+            s.nextLine();
             try (BufferedWriter outfile = new BufferedWriter(new FileWriter(f.getAbsolutePath().replace(".in", ".out")))) {
+                for(int i = 0; i < numOfCoordiantes; i++) {
+                    String[] coordinates = s.nextLine().split(" ");
+                    String[] a = coordinates[0].split(",");
+                    String[] b = coordinates[1].split(",");
+                    Coordinate ac = new Coordinate(Integer.parseInt(a[0]), Integer.parseInt(a[1]));
+                    Coordinate bc = new Coordinate(Integer.parseInt(b[0]), Integer.parseInt(b[1]));
 
+                    if(fieldO.isOnSame(ac, bc)) {
+                        outfile.write("SAME\n");
+                    } else {
+                        outfile.write("DIFFERENT\n");
+                    }
+                }
             }
         }
     }
+
 }
---
+++ ClassicCCC/src/Field.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+public class Field {
+    public List<List<Character>> field;
+
+    public Field(List<List<Character>> field) {
+        this.field = field;
+    }
+    public char getType(int x, int y) {
+        return this.field.get(y).get(x);
+    }
+
+    public boolean isOnSame(Coordinate a, Coordinate b) {
+        if (field.get(a.y()).get(a.x()) != 'L' || field.get(b.y()).get(b.x()) != 'L') {
+            return false;
+        }
+        return depthFirstSearch(new ArrayList<>(field), a,b);
+    }
+    private static boolean depthFirstSearch(List<List<Character>> grid, Coordinate current, Coordinate target) {
+
+        System.out.println(printField(grid));
+        int rows = grid.size();
+        int cols = grid.get(0).size();
+
+        int x = current.x();
+        int y = current.y();
+
+        if (y < 0 || y >= rows || x < 0 || x >= cols || grid.get(y).get(x) == 'W' || grid.get(y).get(x) == 'X') {
+            return false;
+        }
+
+        if (current.equals(target)) {
+            return true;
+        }
+
+        grid.get(y).set(x, 'X'); // Mark the current cell as visited
+
+        // Recursively check adjacent cells
+        List<Coordinate> adjacentCoordinates = List.of(
+                new Coordinate(x - 1, y),
+                new Coordinate(x + 1, y),
+                new Coordinate(x, y - 1),
+                new Coordinate(x, y + 1)
+        );
+
+        for (Coordinate neighbor : adjacentCoordinates) {
+            if (depthFirstSearch(grid, neighbor, target)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public static String printField(List<List<Character>> field) {
+        StringBuilder sb = new StringBuilder();
+        for (List<Character> row : field) {
+            for (Character c : row) {
+                sb.append(c);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (List<Character> row : field) {
+            for (Character c : row) {
+                sb.append(c);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
